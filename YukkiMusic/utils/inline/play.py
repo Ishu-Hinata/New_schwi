6ac81752aa65743754a04f5983f521104ea87243
Ishu <87901742+Ishu-Hinata@users.noreply.github.com>
2022-07-07 19:58:29 +0530

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -27,7 +27,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     buttons = [
         [
             InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
+                text=f"{bar}",
                 callback_data="GetTimer",
             )
         ],
@@ -53,7 +53,7 @@ def telegram_markup_timer(_, chat_id, played, dur):
     buttons = [
         [
             InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
+                text=f"{bar}",
                 callback_data="GetTimer",
             )
         ],
